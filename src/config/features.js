31cef9d4fd1dad1f0f4a462d0ec7075f1a075064
Himanshugulhane27 <himanshugulhane27@gmail.com>
2026-03-09 07:30:00 +0530

    feat: add feature flag configuration

---
+++ src/config/features.js
@@ -0,0 +1,13 @@
+/**
+ * Feature flags for gradual rollout.
+ */
+module.exports = {
+  DARK_MODE: true,
+  CSV_EXPORT: true,
+  RECURRING_TX: true,
+  BUDGET_ALERTS: true,
+  HEALTH_SCORE: true,
+  PDF_EXPORT: false,
+  MULTI_CURRENCY: false,
+  NOTIFICATIONS: false,
+};
